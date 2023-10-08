34106f7afae7c9779d4b249aad213e4a3adde1b8
ailsoya <akalitata53@gmail.com>
2023-10-08 18:10:17 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react'
+
+export class App extends Component {
+  static defaultProps  = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <div>
+          <h2>Please leave feedback</h2>
+          <ul>
+            <li><button type="button" onClick={this.goodClick}>Good</button></li>
+            <li><button type="button">Neutral</button></li>
+            <li><button type="button">Bad</button></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good: {good}</li>
+            <li>Neutral: {neutral}</li>
+            <li>Bad: {bad}</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
